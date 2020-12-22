82bab38b5d7bf768f182e2cead990063ec618b3e
Supuni Uthpala <48378642+supuniuthpala@users.noreply.github.com>
2020-12-22 05:55:26 -0800

    Update ThirdFragment.kt

--- MyApplication/app/src/main/java/com/mobile/myapplication/ThirdFragment.kt
+++ MyApplication/app/src/main/java/com/mobile/myapplication/ThirdFragment.kt
@@ -13,7 +13,6 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.http.POST
 
-
 /**
  * A simple [Fragment] subclass as the third destination in the navigation.
  */
@@ -43,3 +42,5 @@ class ThirdFragment : Fragment() {
         }
 
     }
+
+
